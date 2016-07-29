4ff231d6c05fa529deb9051c18920e8565401e15
MikeH <mike123.henry@gmail.com>
2016-07-29 18:26:01 -0500

    added jawsDB infromation to connection.js

--- config/connection.js
+++ config/connection.js
@@ -13,18 +13,18 @@ var source = {
         database: "burgers_db"
     },
     jawsDB: {
-        Host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-        Username: 'vo7g2cg25tu0lk5j',
-        Password: 'd0hk2hgna2q7p6rp',
+        Host: 'nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        Username: 'rxiw05zby67bvyb0',
+        Password: 'rlj50db4hg3ch9uo ',
         Port: 3306,
-        database: 'q0orx3zic7scjy0p'
+        database: 'fuvq8cyzfx0nmaxp'
         // TODO: Add your JawsDB connection info here
     }
 }
 
 
 // TODO: connect your jawsDB connection with the mysql connection.
-var connection = mysql.createConnection(source.localhost);
+var connection = mysql.createConnection(source.jawsDB);
 
 
 // Replace the connection code here with one to connect to you JawsDB connection.
